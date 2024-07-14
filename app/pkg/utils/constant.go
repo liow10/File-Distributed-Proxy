7bd12544904e05988831cf65b7b7cd9e854c1f50
wang <210761112@qq.com>
2024-07-14 18:13:12 +0800

    FileProxyInitCode

---
+++ app/pkg/utils/constant.go
@@ -0,0 +1,34 @@
+package utils
+
+import "time"
+
+const (
+	Scheme                = "http"
+	WorkID                = "workId"
+	LinkLimit             = 50
+	EncryKey              = "*&^@#$storage"
+	LocalStore            = "D:\\temp\\data\\osproxy\\storage\\localstore"
+	ServiceRedisPrefix    = "service:proxy"
+	ServiceRedisTTl       = time.Second * 3 * 60
+	S3StoragePutThreadNum = 10
+	MultiPartDownload     = 10
+)
+
+// 任务类型
+const (
+	TaskPartMerge = "partMerge"
+)
+
+// 任务状态
+const (
+	TaskStatusUndo    = 0
+	TaskStatusRunning = 1
+	TaskStatusFinish  = 2
+	TaskStatusError   = 99
+)
+
+// worker和队列
+const (
+	MaxWorker = 100
+	MaxQueue  = 200
+)
